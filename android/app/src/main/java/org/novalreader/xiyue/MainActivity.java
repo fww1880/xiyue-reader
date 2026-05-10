5225aa5561695442344a4acc7286e2a23c2a8c75
xiyue-reader <xiyue@novalreader.org>
2026-05-10 18:16:28 +0800

    Fix: remove invalid setWebChromeClient method

--- android/app/src/main/java/org/novalreader/xiyue/MainActivity.java
+++ android/app/src/main/java/org/novalreader/xiyue/MainActivity.java
@@ -1,24 +1,3 @@
 package org.novalreader.xiyue;
-import android.app.Activity;
-import android.content.Intent;
-import android.net.Uri;
-import android.os.Bundle;
-import android.webkit.ValueCallback;
-import android.webkit.WebChromeClient;
-import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
-import com.getcapacitor.BridgeWebChromeClient;
-public class MainActivity extends BridgeActivity {
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        
-        // 自定义 WebChromeClient 以支持文件选择
-        bridge.setWebChromeClient(new BridgeWebChromeClient(bridge) {
-            @Override
-            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
-                return super.onShowFileChooser(webView, filePathCallback, fileChooserParams);
-            }
-        });
-    }
-}
+public class MainActivity extends BridgeActivity {}
